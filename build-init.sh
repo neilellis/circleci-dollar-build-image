d423101df13162ad6c55455226139a6fac335e06
neilellis <neil@vizz.buzz>
2017-07-23 02:43:49 +0100

    Image improvements

--- build-init.sh
+++ build-init.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+set -eux
 cd /home/circleci
 git clone https://github.com/sillelien/build-utils.git
 chmod a+x /home/circleci/build-utils/*.sh
 cd -
-sudo ntpdate -s time.nist.gov
+sudo ntpdate -s time.nist.gov || :
